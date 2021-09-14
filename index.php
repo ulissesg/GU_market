ec555cac123e063d5598b4b8610c3568540a36c4
ulissesg <ulissesg2018@gmail.com>
2021-09-14 15:53:29 -0300

    index refatorado

--- index.php
+++ index.php
@@ -1,25 +1,5 @@
-<?php include_once "src/view/cabecalho_login_signin.php" ?>
-<h3 class="text-center mb-5 fw-light"> login </h3>
+<?php 
 
-<form method="POST" class="m-5 px-5 pb-3 ">
+include_once "src/view/login.php";
 
-    <div class="mx-5 px-5">
-        <div class="mx-5 my-2 px-5">
-            <label class="form-label">Nome</label>
-            <input type="text" name="nome" class="form-control">
-        </div>
-        
-        <div class="mx-5 my-2 px-5">
-            <label class="form-label">Senha</label>
-            <input type="text" name="senha" class="form-control">
-        </div>
-
-        <div class="mx-5 my-2 px-5 text-center">
-            <input type="submit" value="Entrar" class="btn btn-primary m-2">
-            <a class="btn btn-primary m-2" href="/gu_market/src/view/signin.php">Sign in</a>
-        </div>
-        
-    </div>
-    
-</form>
-<?php include_once "src/view/rodape.php" ?>
+?>

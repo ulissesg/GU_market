778d1c8721094cd9ea3a2c2924dde5e405f53469
Ulisses Genguini <42587034+ulissesg@users.noreply.github.com>
2021-09-17 09:00:30 -0300

    teste para windows adicionado

---
+++ test.cmd
@@ -0,0 +1,11 @@
+PATH=%PATH%;C:\xampp\php
+
+CD C:\xampp\htdocs\GU_market\tests
+
+cls
+
+echo .......................... && echo. && php TestUsuario.php && echo. && php TestFornecedor.php && echo. && php TestProduto.php && echo. && php TestCarrinho.php && echo. && echo........................... 
+
+
+pause
+
